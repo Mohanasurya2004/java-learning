a9bfaa288acb25a01b6a72e23bc957f24d058943
Mohanasurya2004 <mohanasurya655@gmail.com>
2025-08-06 17:37:09 +0530

    second commit

---
+++ college.java
@@ -0,0 +1,14 @@
+class college{
+ public static void main(String []args){
+ String college = "VTMT";
+ int code = 113122;
+ int stu_count = 4000;
+ int dept_count = 360;
+ String NAAC_grade = "A++";
+ System.out.println("college:"+college); 
+ System.out.println("code:"+code); 
+ System.out.println("stu_count:"+stu_count); 
+ System.out.println("dept_count:"+dept_count);  
+ System.out.println("NAAC_grade:"+NAAC_grade);
+}
+} 
---
+++ stu_info.java
@@ -0,0 +1,14 @@
+ class Stu_info{
+  public static void main(String[] args){
+  String name = "Surya";
+  long phn = 9159631565L;
+  int totalmark = 450;
+  float percentage = 10.0f;
+  char grade = 'A' ;
+ System.out.println("name:"+name) ;
+ System.out.println("phn:"+phn) ;
+ System.out.println("totalmark:"+totalmark) ;
+ System.out.println("percentage:"+percentage) ;
+ System.out.println("grade:"+grade) ;
+}
+}
