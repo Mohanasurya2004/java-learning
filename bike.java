35b22344c0cc69fcc8a03574b75cae3a4d4f32be
Mohanasurya2004 <mohanasurya655@gmail.com>
2025-08-06 18:10:29 +0530

    third commit

---
+++ bike.java
@@ -0,0 +1,8 @@
+class Bike{
+ public static void main(String []args){
+ String bike_name = "unicorn" ;
+ String owner_name = "surya";
+ System.out.println("bike_name:"+bike_name);
+ System.out.println("owner_name:"+owner_name);
+}
+}
