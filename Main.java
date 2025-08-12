2078ff67d0661a0cff2649ce1ad57bbe21397137
Mohanasurya2004 <mohanasurya655@gmail.com>
2025-08-12 17:49:36 +0530

    operators

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 public class Main {
-public static void main(string []args){
-int items = 50;
-float costPerItem = 9.99f;
-float totalCost = items * costPerItem;
-char currency = '$';
-System.out.println("Number of items:"+items); 
-System.out.println("Cost per item:"+currency+costPerItem); 
-System.out.println("Total cost = "+currency+totalCost);  
+    public static void main(String[] args) {
+        int items = 50;
+        float costPerItem = 9.99f;
+        float totalCost = items * costPerItem;
+        char currency = '$';
+        System.out.println("Number of items:" + items);
+        System.out.println("Cost per item:" + currency + costPerItem);
+        System.out.println("Total cost = " + currency + totalCost);
+    }
+}
